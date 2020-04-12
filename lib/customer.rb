dd788ef81a9d5ee3e2aef2111842e4c36766b9c2
Takashi OGURA <takto78@gmail.com>
2020-04-12 22:57:01 +0900

    Add Rental and Customer

---
+++ lib/customer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Customer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @rentals = []
+  end
+
+  def add_rental(arg)
+    @rentals << arg
+  end
+end
---
+++ lib/rental.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Rental
+  attr_reader :movie, :days_rented
+
+  def initialize(movie, days_rented)
+    @movie, @days_rented = movie, days_rented
+  end
+end
